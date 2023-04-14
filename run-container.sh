af4149fe5275ba54840ae6d19ef0b23f5bf1f0a4
Aziz Jouini <aziz@localhost.localdomain>
2023-04-14 14:15:49 +0200

    updated image

---
+++ run-container.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+git add . 
+git commit -m "updated image"
+git push origin 
+docker compose stop
+docker image rm azizjouini/passport-app-saml --force
+docker compose rm -f
+docker compose pull   
+docker compose up -d

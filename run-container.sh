0cb32ffdbc2d6094dc74fa5478f513de96e9980f
Aziz Jouini <aziz@localhost.localdomain>
2023-04-14 14:41:43 +0200

    updated image

--- run-container.sh
+++ run-container.sh
@@ -7,6 +7,6 @@ git push origin
 docker compose stop
 docker image rm azizjouini/passport-app-saml --force
 docker compose rm -f
-sleep 20s
+curl -v -X POST 'https://new-image.free.beeceptor.com/my/api/path' -H 'Content-Type: application/json' -d '{"data":"Hello Beeceptor"}'
 docker compose pull   
 docker compose up -d

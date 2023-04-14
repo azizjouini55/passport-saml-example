8608ae94329a72a8d8a48f329db3f654ac209e17
Aziz Jouini <aziz@localhost.localdomain>
2023-04-14 16:00:09 +0200

    updated image

--- run-container.sh
+++ run-container.sh
@@ -1,8 +1,7 @@
 
 #!/bin/bash
-
-SLEEP_TIME=60s
-
+LOCAL_IMAGE_DIGEST=docker inspect --format='{{index .RepoDigests 0}}' azizjouini/passport-app-saml
+SLEEP_TIME=20s
 git add . 
 git commit -m "updated image"
 git push origin 
@@ -11,7 +10,17 @@ docker compose stop
 docker image rm azizjouini/passport-app-saml --force
 docker compose rm -f
 
+while true; do
+DOCKERHUB_DIGEST=docker manifest inspect azizjouini/passport-app-saml | jq -r '.config.digest'
+if ["$DOCKERHUB_DIGEST" -eq "$LOCAL_IMAGE_DIGEST"]
+ echo "new image is pushed"
+ break
+else
+ echo "no image pushed"
+ fi
+
 sleep $SLEEP_TIME
+done
 
 docker compose pull   
 docker compose up -d

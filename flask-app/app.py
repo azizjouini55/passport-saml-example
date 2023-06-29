c7ab72c318802ced12af955ed8e036498cb9530d
Aziz Jouini <aziz@localhost.localdomain>
2023-06-29 17:33:23 +0200

    traefik reverse proxy functional

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,12 +5,6 @@ app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'super-secret' # replace with your own secret key
 jwt = JWTManager(app)
 
-# login  (test) piproute
-# @app.route('/auth', methods=['POST'])
-# def login():
-#     # authenticate user and generate JWT
-#     access_token = create_access_token(identity='username')
-#     return jsonify(access_token=access_token)
 
 @app.route('/', methods=['GET'])
 def test():
